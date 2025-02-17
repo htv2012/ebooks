ad3190a6bf8fb99a74f16a0ab1898931f20341fd
Hai Vu <haivu2004@gmail.com>
2025-02-16 21:25:22 -0800

    batch 20

--- commit.py
+++ commit.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 """Commit the next edit batch"""
 
-import subprocess
+from subprocess import run
+
 
 def main():
-    p = subprocess.run(
-        ["git", "log", "-1", "--pretty=%B"],
-        capture_output=True,
-        text=True,
-    )
-    num = int(p.stdout.removeprefix("ddsl").removeprefix("batch ")) + 1
-    subprocess.run(["git", "commit", "-a", "-m", f"batch {num}"])
-    subprocess.run(['git', 'push'])
+    p = run(["git", "log", "-1", "--pretty=%B"], capture_output=True)
+    num = int(p.stdout.removeprefix(b"batch ")) + 1
+    run(["git", "commit", "-a", "-m", f"batch {num}"])
+    run(["git", "push"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
