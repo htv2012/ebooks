de50258335e148bf564df9d5217334c57e1ef482
Hai Vu <haivu2004@gmail.com>
2025-11-05 15:58:34 -0800

    ai_bac_nhip_cau 2025-11-05 15:58

--- commit.py
+++ commit.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 """Commit the next edit batch"""
 
+import collections
 from subprocess import run
 import datetime
+import pathlib
+import collections
 
+
+def get_modified_dir():
+    process = run(["git", "status", "--porcelain"], text=True, capture_output=True, check=False)
+    dirs = collections.Counter(
+        pathlib.Path(line.split()[-1]).parent.name
+        for line in process.stdout.splitlines()
+    )
+    common_dir, count =  dirs.most_common(1)[0]
+    return common_dir
 
 def main():
+    modified_dir  = get_modified_dir()
     now = datetime.datetime.now()
-    message = now.strftime('Batch %Y-%m-%d %H:%M')
+    message = modified_dir + now.strftime(' %Y-%m-%d %H:%M')
     run(["git", "commit", "-a", "-m", message])
     run(["git", "push"])
 
